6742c7683de1867094b4a5f5416e54c4ba4535fd
Anand <msanand2005@gmail.com>
2025-09-24 00:43:30 +0530

    sec1

--- backend/index.js
+++ backend/index.js
@@ -85,8 +85,12 @@ const questions = [
   },
   {
     id: 6,
-    question:
-      "Location: clue is in 'pItFlick lights guide you — the capitals and enclosure are shining; follow (2) beams to the clue'",
+    question: (
+      <>
+        Location: clue is in 'l<em>I</em>t<em>F</em>lick lights guide you — the
+        capitals and enclosure are shining; follow (2) beams to the clue'
+      </>
+    ),
     answer: "if2",
     clue: "It's definitely not in CSE. IT maybe",
   },
